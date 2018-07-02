c41cfb906cf79cc1c94de148fbf106691a7728fd
BeryNice <berenice.lemus@gmail.com>
2018-07-02 17:32:18 -0500

    agregando el archivo pagina-principal.htm con la estructura en bootstrap, actualizando e logo de laboratoria, agregando estilos personalizados en style.css y editando el archivo data.js para test

--- src/data.js
+++ src/data.js
@@ -1,30 +1,60 @@
 // Esta hoja debe contener toda la funcionalidad que corresponda a obtener, procesar y manipular datos.
-/*
-window.computeGenerationsStats = (laboratoria) => {
-    const generacionesArr = [];
-    const obj = {
-        campus : '',
-        generation: ' ',
-        average : 0,
-        count : 0,
-    };
 
-    let average = 0;
-    for (key in laboratoria) {
-        obj.campus = key;
-        const generations = Object.keys([laboratoria[key].generacion]);
-        generations.forEach((generation) => {
-            obj.generation = generation;
-            const students = laboratoria[key].generacion[generation].estudiantes;
-            for (student in students) {
-                average += students[student].progreso.porcentajeCompletado;
-                average = average / students.length;
-                obj.average = average;
-                obj.count = students.length;
-            generacionesArr.push(obj);
-            }
-        })
-    }
-     return generacionesArr;
-    }
+window.computeGenerationsStats = (data) => {
+// 1 debería retornar un arreglo de generaciones con propiedad average y count
+// 2 generation para la primera generación en data de prueba - ver carpeta data/
+// 2.1 debería tener una propiedad average con valor 75
+// 2.2 debería tener una propiedad count con valor 15
+
+  /*    const generacionesArr = [];
+  const obj = {
+  campus : '',
+  generation: ' ',
+  average : 0,
+  count : 0,
+};
+  let average = 0;
+  for (key in laboratoria) {
+    obj.campus = key;
+    const generations = Object.keys([laboratoria[key].generacion]);
+    generations.forEach((generation) => {
+      obj.generation = generation;
+      const students = laboratoria[key].generacion[generation].estudiantes;
+      for (student in students) {
+        average += students[student].progreso.porcentajeCompletado;
+        average = average / students.length;
+        obj.average = average;
+        obj.count = students.length;
+        generacionesArr.push(obj);
+      }
+    })
+  }
+return generacionesArr; */
+
+};
+
+window.computeStudentsStats = (data) => {
+// 1 debería retornar arreglo de students con propiedad campus y propiedad generation
+// 2 debería retornar arreglo de students con propiedad stats
+// 3 student.stats para el primer usuario en data de prueba - ver carpeta data
+// 3.1 debería tener propiedad completedPercentage con valor 89
+// 3.2 debería tener propiedad completedPercentage dentro de propiedad topics con valor 80
+// 3.3 debería tener propiedad percentageDuration dentro de propiedad topics con valor 79
+// 3.4 debería tener propiedad subtopics que es un objeto con primera key "0-bienvenida-orientacion" con valor {completado: 1, duracionSubtema: 55, tipo: "lectura"}
+/*
+const topics = Object.keys(processed[0].stats.topics);
+const subTopics = Object.keys(processed[0].stats.topics[topics[0]].subtopics);
 */
+
+  for (item in data){
+    console.log(Object.keys(data[item]));
+  };
+};
+
+window.sortStudents = (data) => {
+
+};
+
+window.filterStudents = (data) => {
+
+};
--- src/main.js
+++ src/main.js
@@ -1,42 +1,48 @@
 //Aquí poner todo el código que tenga que ver con mostrar los datos en la pantalla
 
-document.getElementById('jsonBtn').addEventListener('click', mainData);
+// document.getElementById('jsonBtn').addEventListener('click', mainData);
+document.getElementById('sedesBtn').addEventListener('click', mainData);
+
 //Variable que voy a usar para pintar data
 let contenido = document.getElementById("contenido");
 const url = 'https://raw.githubusercontent.com/BeryNice/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
- function mainData() {
-        fetch(url)
-        .then (response => (response.json())
-        .then ((data) => {
-                    const laboratoriaSedes = getSedes (data);
-                    const labGeneraciones = getGeneracion (data);
-        })
-        .catch((error) => {
-            console.log('Error: ', error);
-        }));
-    }
+function mainData() {
+  fetch(url)
+  .then (response => (response.json())
+  .then ((data) => {
+    const laboratoriaSedes = getSedes (data);
+    const labGeneraciones = getGeneracion (data);
+    const labStudents = computeStudentsStats(data);
+  })
+  .catch((error) => {
+    console.log('Error: ', error);
+  }));
+}
+
+
 //OBTENIENDO LAS SEDES
 const getSedes = (data) => {
-    for (item in data) {
-     //console.log(item);
-     const p = document.createElement('p');
-     const sedes = item;
-     //console.log(sedes);
-     const containerSedes = document.getElementById('resultados');
-     p.innerHTML = sedes;
-     containerSedes.appendChild(p);
-    }
+  for (item in data) {
+    //console.log(item);
+    const p = document.createElement('p');
+    const sedes = item;
+    //console.log(sedes);
+    const containerSedes = document.getElementById('resultados');
+    p.innerHTML = sedes;
+    containerSedes.appendChild(p);
+  }
 };
 
 //OBTENIENDO LAS GENERACIONES
 const getGeneracion = (data) => {
-    for (item in data) {
-      const p = document.createElement('p');
-      const generaciones = Object.keys(data[item].generacion);
-      console.log(generaciones);
-      /*const containerGeneraciones = document.getElementById('resultados');
-      p.innerHTML = generaciones;
-      containerGeneraciones.appendChild(p);*/
-    };
+  for (item in data) {
+    const p = document.createElement('p');
+    const generaciones = Object.keys(data[item].generacion);
+    //console.log(generaciones);
+    const containerGeneraciones = document.getElementById('resultados');
+    p.innerHTML = generaciones;
+    containerGeneraciones.appendChild(p);
+  };
+}
 
-    }
+//PINTANDO ESTUDIANTES
